59d7b2e844658b6644dfd1247f9a3bed5b8a7175
ThanabutJ <thanabut.j@gmail.com>
2021-07-30 02:08:56 +0700

    add annotation to proto

--- gen-proto.sh
+++ gen-proto.sh
@@ -1,4 +1,6 @@
 protoc -I ./proto/ \
+    -I include/googleapis -I include/grpc-gateway \
     --go_out=. --go_opt=module=github.com/thanabutj/gen-openapi-from-grpc \
     --go-grpc_out=. --go-grpc_opt=module=github.com/thanabutj/gen-openapi-from-grpc \
+    --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true \
     proto/notes/v1/*.proto
